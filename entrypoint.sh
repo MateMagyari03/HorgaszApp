a64a706d36d7c06403f0a08d90bd8534945ffd0f
Magyari Máté <h267383>
2025-12-14 15:56:21 +0100

    feat: Set up GitHub Actions CI/CD and production Docker deployment with secret management and updated configurations.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 set -e
 
-mkdir -p /run/nginx /var/log/nginx /var/log/php-fpm # Ha nem léteznéánek
+mkdir -p /run/nginx /var/log/nginx /var/log/php-fpm
 
-php-fpm82 -F &
+if [ -f /run/secrets/app_key ]; then
+    export APP_KEY=$(cat /run/secrets/app_key)
+else
+    echo "App Key does not exist or is not readable"
+fi
+
+if [ -f /run/secrets/mail_password ]; then
+    export MAIL_PASSWORD=$(cat /run/secrets/mail_password)
+else
+    echo "Mail Password does not exist or is not readable"
+fi
+
+php-fpm84 -F &
+
+php /var/www/html/artisan down
+php /var/www/html/artisan migrate --force
+php /var/www/html/artisan up
 
 exec nginx -g "daemon off;"
