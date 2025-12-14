894ac6be62ff75579f2c2e9682a55d82ed825797
Magyari Máté <h267383>
2025-12-14 20:29:49 +0100

    feat: Enforce HTTPS in production and update the application name.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,12 +8,14 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        
+
     }
 
-    
+
     public function boot(): void
     {
-        
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
